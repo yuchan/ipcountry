5f6ba584569417a31f87cc229a84aff7a3ee21bb
Yusuke Ohashi <yusuke@junkpiano.me>
2014-11-20 21:29:16 +0900

    bump version v0.0.7

--- Rakefile
+++ Rakefile
@@ -1,2 +1,10 @@
 require "bundler/gem_tasks"
+require "rake/testtask"
+
+Rake::TestTask.new(:test) do |t|
+  t.libs << "test"
+end
+
+task :default => :test
+
 
---
+++ test/test_ipcountry.rb
@@ -0,0 +1,9 @@
+require 'minitest_helper'
+
+class TestIpcountry < MiniTest::Unit::TestCase
+  def test_that_it_has_a_version_number
+    refute_nil ::Ipcountry::VERSION
+  end
+end
+
+
